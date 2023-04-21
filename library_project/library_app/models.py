1f1f12e9d02ad9e9fe72a86c20c4b491ad21de1e
Erika <kikayus@hotmail.com>
2023-04-21 18:24:56 +0200

    first commit

---
+++ library_project/library_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    genre = models.CharField(max_length=200)
+    year = models.IntegerField()
+    
+    class Meta:
+        db_table = 'books'
+        
